5a19f2536c8b04fc3996019a85125e7bdffea8c0
Nihar Agrawal <ngagrawa@usc.edu>
2017-08-12 16:15:08 -0700

    Initial Commit

---
+++ GIF_Tracks_3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GIF_Tracks_3
+//
+//  Created by Nihar Agrawal on 8/12/17.
+//  Copyright © 2017 Nihar Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
